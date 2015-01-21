a03766f0ecd372275aaba695b7f56eb1fdb8b0ac
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-01-21 11:14:16 +0100

    update

--- esame 14-01-2014/esercizio3.js
+++ esame 14-01-2014/esercizio3.js
@@ -1,15 +1,23 @@
-function statistica(anno){
+function statistica(){
 	httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = riempiTabella;
-	httpRequest.open("GET","xml.xml",true);
-	httpRequest.send("");
+	httpRequest.open("GET", "xml.xml", true);
+	httpRequest.send();
 }
 
 function riempiTabella () {
 	if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-		var nodo1 = httpRequest.responseXML.documentElement.getElementsByTagName("response");
-		var anno = nodo1[0].getElementsByTagName("anno").firstChild.nodeValue;
-		var output = '<table id="tb1" border="1"><thead><tr><th colspan="2">' + anno + '</th></tr><tr><th>Mese</th><th>Pioggia<br/>Totale</th></tr></thead><tbody></tbody></table>';
-		document.body.innerHTML = output;
+        var type = httpRequest.getResponseHeader("Content-Type");
+		// Check type so we don't get HTML documents in the future
+		if (type.indexOf("xml") !== -1 && httpRequest.responseXML)
+			alert('xml');
+		else if (type === "application/json")
+			alert('json');
+		else
+			alert('text');
+		var xmlDoc = httpRequest.responseXML;
+		var response=xmlDoc.getElementsByTagName("anno");
+		var anno=response[0].text;
+		alert(anno);
 	}
 }
