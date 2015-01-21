69232233853d8f21297c3a55cbc14b36c23ce3ea
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-01-21 12:47:43 +0100

    update

--- esame 06-02-2014/esercizio4.js
+++ esame 06-02-2014/esercizio4.js
@@ -1,6 +1,7 @@
 function creaCookie(){
 	var scadenza = new Date();
-	var now = new Date();
-	scadenza.setTime(now.getTime() + (4 * 60000)); //durata 4 giorni dal 6 al 10
+    scadenza.setDate(10);
+    scadenza.setMonth(2);
+    scadenza.setFullYear(2014);
 	document.cookie = 'METEO' + '=' + 'SALERNO' + '; expires=' + scadenza + ': path=' + 'METEO.COM/SUD'
 }
