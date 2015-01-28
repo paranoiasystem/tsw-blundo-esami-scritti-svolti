a45b3590974e346fffffb58ff7fa1e5be3ba6cd6
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-01-28 11:52:38 +0100

    development

--- esame 20-06-2013/esame.php
+++ esame 20-06-2013/esame.php
@@ -7,31 +7,34 @@
 			alert(id);
 		}
 	</script>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <?php
-	if(!isset($_COOKIE['fsdf']) || $_COOKIE['fsdf'] != "2xs"){
+	/*if(!isset($_COOKIE['fsdf']) || $_COOKIE['fsdf'] != "2xs"){
 		echo "Errore";
-		exit();
-	} else if (isset($_COOKIE['fsdf']) && $_COOKIE['fsdf'] == "2xs"){
+	} else if (isset($_COOKIE['fsdf']) && $_COOKIE['fsdf'] == "2xs"){*/
 		$connect = mysql_connect("localhost", "root", "") or die ("Errore nella conessione");
-		mysql_select_db("magazzino", $connect) or die("Errore nella query");
-		$query = mysql_query("SELECT * FROM merce");
+		mysql_select_db("magazzino", $connect) or die("Errore nella selezione");
+		$query = mysql_query("SELECT * FROM merce") or die("Errore nella query");
 ?>
-	<table>
-		<thead><td>Prodotto</td><td>Prezzo Massimo</td><td>Prezzo Minimo</td></thead>
-		<tbody>
+	<div id="cont">
+		<table border="1">
+			<thead><tr><td>Prodotto</td><td>Prezzo Massimo</td><td>Prezzo Minimo</td></tr></thead>
+			<tbody>
 <?php
 		$i = 0;
 		while($row = mysql_fetch_array($query)){
-			echo '<tr id="' . $i . '" onclick=mostra(' . $i .')><td>' . $row[0] . '</td><td>' . $row[1] . '</td><td>' . $row [2] . '</td></tr>';
-			$i++;
+		echo '<tr id="' . $i . '" onclick=mostra(' . $i .')><td>' . $row[0] . '</td><td>' . $row[1] . '</td><td>' . $row [2] . '</td></tr>';
+		$i++;
 		}
 ?>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+		<p>Merce in magazzino</p>
+	</div>
 <?php
-	}
+	//}
 ?>
 </body>
 </html>
