fa59cfac308c56ce5b6e2418cf08ef6f781397d8
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-02-24 12:39:10 +0100

    problema class

--- esame 04-02-2015/getData.php
+++ esame 04-02-2015/getData.php
@@ -5,6 +5,7 @@
 	mysql_select_db("my_mfwebitaly", $con);
 	$ris = mysql_query("SELECT * FROM modelli WHERE modello = '" . $_GET['m'] . "'");
 	$row = mysql_fetch_array($ris);
+	$json
 	$xml .= "<modello>" . $row[0] . "</modello><mis_27>" . $row[1] . "</mis_27><mis_29>" . $row[2] . "</mis_29>";
 	$xml .= "</response>";
 	echo $xml;
